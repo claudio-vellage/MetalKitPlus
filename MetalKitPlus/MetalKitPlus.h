009c513a50247e07663f80a915ecad31d56527a0
Claudio Vellage <claudio.vellage@rub.de>
2016-11-11 22:23:51 +0100

    Initial Commit

---
+++ MetalKitPlus/MetalKitPlus.h
@@ -0,0 +1,13 @@
+//
+//  MetalKitPlus.h
+//  MetalKitPlus
+//
+//  Created by Claudio Vellage on 11.11.16.
+//  Copyright © 2016 Claudio Vellage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MetalKitPlus : NSObject
+
+@end
---
+++ MetalKitPlus/MetalKitPlus.m
@@ -0,0 +1,13 @@
+//
+//  MetalKitPlus.m
+//  MetalKitPlus
+//
+//  Created by Claudio Vellage on 11.11.16.
+//  Copyright © 2016 Claudio Vellage. All rights reserved.
+//
+
+#import "MetalKitPlus.h"
+
+@implementation MetalKitPlus
+
+@end
