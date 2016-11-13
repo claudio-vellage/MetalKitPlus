d717803f0a6bd141d26a7f94d7425f1fd46cf27e
Claudio Vellage <claudio.vellage@rub.de>
2016-11-13 20:14:41 +0100

    Changed project type

--- MetalKitPlus/MetalKitPlus.h
+++ MetalKitPlus/MetalKitPlus.h
@@ -2,12 +2,18 @@
 //  MetalKitPlus.h
 //  MetalKitPlus
 //
-//  Created by Claudio Vellage on 11.11.16.
+//  Created by Claudio Vellage on 13.11.16.
 //  Copyright © 2016 Claudio Vellage. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
-@interface MetalKitPlus : NSObject
+//! Project version number for MetalKitPlus.
+FOUNDATION_EXPORT double MetalKitPlusVersionNumber;
 
-@end
+//! Project version string for MetalKitPlus.
+FOUNDATION_EXPORT const unsigned char MetalKitPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalKitPlus/PublicHeader.h>
+
+
