7cd5b93faa71df42a283fa84da4a1c7e1ff0de33
Claudio Vellage <claudio.vellage@rub.de>
2017-03-03 23:07:00 +0100

    Added stub for MTKDevice and added Tests for MTKDevice

---
+++ MetalKitPlus/MTKDevice.swift
@@ -0,0 +1,19 @@
+//
+//  MTKDevice.swift
+//  MetalKitPlus
+//
+//  Created by Claudio Vellage on 03.03.17.
+//  Copyright © 2017 Claudio Vellage. All rights reserved.
+//
+
+import Metal
+
+private let _instance = MTKDevice()
+
+class MTKDevice {
+    public let device = MTLCreateSystemDefaultDevice()
+    
+    public class var instance : MTKDevice {
+        return _instance
+    }
+}
